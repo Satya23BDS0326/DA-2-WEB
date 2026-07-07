afe140aa48d693c947752fe6b094b83fd0819900
Balla Satya Narayana <balla.veera2023@vitstudent.ac.in>
2026-07-07 23:05:20 +0530

    Fix localStorage method typo and clean up code

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-let expenses = JSON.parse(localStorage.gtItem("expenses")) || [];
+let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
 const expenseList = document.getElementById("expense-list");
 const totalAmount = document.getElementById("total-amount");
 
