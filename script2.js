2a828248f8320f7105df3ecf591799d0ba60a6d8
Balla Satya Narayana <balla.veera2023@vitstudent.ac.in>
2026-07-11 22:55:19 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,4 +1,4 @@
-lt expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
 const expenseList = document.getElementById("expense-list");
 const totalAmount = document.getElementById("total-amount");
 
